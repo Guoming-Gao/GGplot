8fd6dda1ab2f232e2b2ffb49e606e0b1b62f9fbe
Guoming <51248407+Guoming-Gao@users.noreply.github.com>
2022-09-01 00:44:34 -0400

    D histogram with theoretical D lower bounds

---
+++ GGplot-histplot-D-staticRNAcontrol.py
@@ -0,0 +1,51 @@
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from copy import deepcopy
+import seaborn as sns
+
+sns.set(color_codes=True, style="white")
+
+###############################################
+# Loading Data
+R2threshold = 0.9
+
+folderpath = "/Volumes/AnalysisGG/PROCESSED_DATA/DiffusionCoeffcientLimit"
+os.chdir(folderpath)
+labels = ["2 s", "100 ms", "40 ms", "20 ms"]
+fname_starts = ["FL_2s", "FL_100ms", "FL_40ms", "FL_20ms"]
+
+lst_dataclass = []
+lst_log10D = []
+for label, start in zip(labels, fname_starts):
+    lst_df = [
+        pd.read_csv(f)
+        for f in os.listdir(folderpath)
+        if f.endswith("linregress_D.csv") & f.startswith(start)
+    ]
+    for df in lst_df:
+        df_R2above = df[df["R2"] >= R2threshold]
+        lst_log10D.extend(list(df_R2above["log10D"]))
+        lst_dataclass.extend(list(np.repeat(label, df_R2above.shape[0])))
+
+df_plot = pd.DataFrame({"log10D": lst_log10D, "Exposure Time": lst_dataclass},)
+df_plot = df_plot.astype(dtype={"log10D": "float64", "Exposure Time": "string"})
+
+plt.figure(figsize=(9, 4), dpi=200)
+sns.histplot(
+    data=df_plot,
+    x="log10D",
+    hue="Exposure Time",
+    common_norm=False,
+    stat="probability",
+)
+# Theoretical Thresholds
+lst_exptime = [2, 0.1, 0.04, 0.02]
+for exptime, idx in zip(lst_exptime, range(4)):
+    bound = np.log10((0.050 ** 2) / (4 * exptime))
+    plt.axvline(x=bound, color=sns.color_palette()[idx], ls="--")
+plt.title("D lower bounds by imaging frequency")
+plt.xlabel("log10D ($\mu$m^2/s)")
+plt.tight_layout()
+plt.savefig("D lower bounds by imaging frequency.png", format="png")
